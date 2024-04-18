70f89e3c4ca2ad2f527c5d0864f29dda4caab8c7
seb-rdz <A01709461@tec.mx>
2024-04-17 19:40:53 -0600

    added "fare" histogram

--- hola.py
+++ hola.py
@@ -3,7 +3,7 @@
 # Importamos la librería Streamlit
 import streamlit as st
 import pandas as pd
-#import matplotlib.pyplot as ptl
+import matplotlib.pyplot as plt
 
 # Crear el título para la aplicación web
 st.title("Titanic App")
@@ -13,3 +13,8 @@ st.write("A01709461")
 titanic_link = 'titanic(1).csv'
 titanic_data = pd.read_csv(titanic_link)
 st.dataframe(titanic_data)
+
+fig, ax = plt.subplots() 
+ax.hist(titanic_data.Fare) 
+st.header("Histograma del Titanic") 
+st.pyplot(fig)
